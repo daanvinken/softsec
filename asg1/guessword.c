5ef29abb83cc4c539f11736f912fc79c2cb8a947
daanvinken <vinkendaan@gmail.com>
2021-09-12 16:42:30 +0200

    reading lines from file

--- asg1/guessword.c
+++ asg1/guessword.c
@@ -20,36 +20,75 @@
 *******************************************************************************/
 #define MD5 "$1$M9"
 #define SALT "M9"
-#define CHUNKSIZE 50
+#define CHUNKSIZE 100
 
 /******************************************************************************
 * Main code
 *******************************************************************************/
 
-void read_file(void *filepath){
-    FILE *f = fopen("training-shadow.txt", "r");
+char** get_file_lines(void *filepath){
+    FILE *f = fopen(filepath, "r");
     if(f == NULL) {
         perror("Error reading input file!");
         exit(1);
     }
 
-    char single_read[CHUNKSIZE];
-
-    while(fgets(single_read, sizeof(single_read), f) != NULL) {
-        printf("%s", single_read);
+    // Count line numbers for array of lines (we load them in memory right away)
+    int character = 0;
+    int num_lines = 0;
+    while(!feof(f))
+    {
+        character = fgetc(f);
+        if(character == '\n')
+            num_lines++;
     }
+
+    // TEMP
+    // num_lines = 10;
+    printf("%d lines found.\n", num_lines);
+
+    // Malloc output array of strings
+    char** input_file_lines = malloc(num_lines * sizeof(char*) * CHUNKSIZE);
+    int i = 0;
+    for (i =0 ; i < num_lines; ++i)
+        input_file_lines[i] = malloc(CHUNKSIZE * sizeof(char));
+
+    // Reading buffer
+    char * single_read[CHUNKSIZE];
+
+    // Close and re-open file (EOF)
+    fclose(f);
+    f = fopen(filepath, "r");
+    i = 0;
+
+    // Read file and store in array
+    while(fgets(&single_read, CHUNKSIZE, f) != NULL && i <= num_lines) {
+        strcpy(input_file_lines[i], single_read);
+        i++;
+    }
+    printf("\n");
     fflush(stdout);
     fclose(f);
+
+    //FREE MEMORY
+    return input_file_lines;
+
 }
 
-// int main(int argc, char const *argv[])
+
 int main()
 {
     const char string[] = "iloveyou";
     char * hash = crypt(string, MD5);
     printf("%s\n",hash);
+    /* **************************** */
 
-    read_file("testing-shadow.txt");
+    char** lines = get_file_lines("training-passwd.txt");
+    for (int i = 0; i < 15; i++)
+    {
+        printf("%s", lines[i]);
+        printf("\n");
+    }
 
     return 0;
 }
